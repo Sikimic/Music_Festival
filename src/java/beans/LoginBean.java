1a0c32b9409ff0b9ff4afe6781988c164755c9e4
Sikimic Nebojsa <sikimic94@gmail.com>
2017-02-02 15:53:24 +0100

    Add User mapped from database

--- src/java/beans/LoginBean.java
+++ src/java/beans/LoginBean.java
@@ -12,16 +12,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
-import models.User;
+import entities.User;
 
 import org.hibernate.Session;
 import org.hibernate.*;
+import utils.HibernateHelper;
 
 
 /**
@@ -40,72 +43,35 @@ public class LoginBean {
     }
     
     public String doLogin() {
-        
         String res = "";
+        List<Object> list = new ArrayList<>();
+        Session session = HibernateHelper.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+
         
-        Connection conn = DB.getInstance().getConnection();
-        try {
-            Statement stmt = conn.createStatement();
-            String query = "SELECT * FROM user WHERE username= '" + username +"' AND password = '" + password +"'";
-            ResultSet rs = stmt.executeQuery(query);
-            
-            if (rs.next()) {
-                user.setEmail(rs.getString("email"));
-                user.setIme(rs.getString("ime"));
-                user.setPassword(password);
-                user.setPrezime("prezime");
-                user.setRepassword(password);
-                user.setTelefon(rs.getString("telefon"));
-                user.setUsername(username);
-                
-                HttpSession session = AppController.getSession();
-                session.setAttribute("user", user);
-                res = "homePage";
-            }
-            
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            DB.getInstance().putConnection(conn);
-        }
+        Query query = session.createQuery("SELECT username, password, ime, prezime, telefon, email, repassword FROM user WHERE username = :username AND password = :password");
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        list = query.list();
+        if (list != null) {
+            session.getTransaction().commit();
+            user.setUsername((String)list.get(0));
+            user.setPassword((String)list.get(1));
+            user.setIme((String)list.get(2));
+            user.setPrezime((String)list.get(3));
+            user.setTelefon((String)list.get(4));
+            user.setEmail((String)list.get(5));
+            user.setRepassword((String)list.get(6));
+            res = "homePage";
+        } 
+       
+
         return res;
     }
     
     public String doRegister() {
         String res = "";
-        if (!user.getPassword().equals(user.getRepassword())) return res;
-        Connection conn = DB.getInstance().getConnection();
-        try {
-            
-            String query = "INSERT INTO user (username,password,ime,prezime,telefon,email,repassword) VALUES (?,?,?,?,?,?,?)";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getPassword());
-            stmt.setString(3, user.getIme());
-            stmt.setString(4, user.getPrezime());
-            stmt.setString(5, user.getTelefon());
-            stmt.setString(6, user.getEmail());
-            stmt.setString(7, user.getPassword());
-            stmt.execute();
-               
-            res = "index";
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            DB.getInstance().putConnection(conn);
-        }
+       
         return res;
     }
     
